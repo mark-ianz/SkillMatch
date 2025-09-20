3e25ed82fa4ac3ec4fc7bd874b231f6258ee1b98
mark-ianz <bustillomarkian23@gmail.com>
2025-09-20 22:47:01 +0800

    nag save ba? HAHA

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Totoo na to tangina. 
+            Totoo na to tangina. Last na gago.
           </li>
         </ol>
 
