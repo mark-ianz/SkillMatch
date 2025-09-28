723649721c8727e312f3e2525d414ca9e393db68
mark-ianz <bustillomarkian23@gmail.com>
2025-09-29 03:36:25 +0800

    Add JoinNowButton component for user sign-in navigation

---
+++ src/app/_components/common/button/JoinNowButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function JoinNowButton() {
+  const router = useRouter();
+
+  const handleJoinNow = () => {
+    router.push("/auth/signin");
+  };
+
+  return <Button onClick={handleJoinNow}>Join Now</Button>;
+}
