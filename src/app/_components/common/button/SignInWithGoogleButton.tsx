74b999bbadf91be090321b2d328a4a574417ef59
mark-ianz <bustillomarkian23@gmail.com>
2025-09-29 03:36:34 +0800

    Add SignInWithGoogleButton component for Google sign-in functionality

---
+++ src/app/_components/common/button/SignInWithGoogleButton.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import React from "react";
+
+export default function SignInWithGoogleButton() {
+  return (
+    <Button
+      onClick={async () => {
+        const result = await signIn("google", { redirect: false });
+        console.log(result)
+      }}
+      className="bg-skillmatch-primary-light text-skillmatch-dark border hover:bg-skillmatch-muted-light"
+    >
+      <Image
+        src={"/logo/GoogleLogo.png"}
+        alt="Google Logo"
+        width={16}
+        height={16}
+      />
+      Sign in with Google
+    </Button>
+  );
+}
