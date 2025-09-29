e4b396270b1eed6edbe9e9a7bc1552a96182e70d
mark-ianz <bustillomarkian23@gmail.com>
2025-09-30 03:45:29 +0800

    Add TypographyBlockquote, Alert, and Toaster components from shadcn

---
+++ src/app/_components/common/typography/TypographyBlockquote.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export default function TypographyBlockquote({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>
+  );
+}
