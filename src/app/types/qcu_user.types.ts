eac9f652759bccf4f8ad4bc8e717e3805270b230
mark-ianz <bustillomarkian23@gmail.com>
2025-10-06 03:39:30 +0800

    Add QCU_User type definition for user data structure

---
+++ src/app/types/qcu_user.types.ts
@@ -0,0 +1,11 @@
+export type QCU_User = {
+  student_number: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  middle_name: string;
+  course: string;
+  college: string;
+  address: string;
+  created_at: Date;
+}
