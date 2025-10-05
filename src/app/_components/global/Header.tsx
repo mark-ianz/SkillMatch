8fe81749c27a2c5f55971b566e6d8f1df0a16aa9
mark-ianz <bustillomarkian23@gmail.com>
2025-10-06 03:40:55 +0800

    Refactor Header component layout for improved structure and styling

--- src/app/_components/global/Header.tsx
+++ src/app/_components/global/Header.tsx
@@ -10,55 +10,60 @@ import {
 import Link from "next/link";
 import React from "react";
 import JoinNowButton from "../common/button/JoinNowButton";
+import PaddedWrapper from "./PaddedWrapper";
 
 export default function Header() {
   return (
-    <header className="border-b flex items-center justify-between p-6 px-96 h-20">
-      <Link href={"/"} className="font-sans font-semibold text-2xl">SkillMatch</Link>
-      <div className="flex items-center gap-4">
-        <NavigationMenu viewport={false}>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={navigationMenuTriggerStyle()}
-              >
-                <Link href="/">Home</Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Explore</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink asChild>
-                  <Link href="/explore?category=jobs" className="w-[200px]">
-                    <div className="font-medium">Jobs</div>
-                    <div className="text-muted-foreground">
-                      Browse all job listings.
-                    </div>
-                  </Link>
+    <header className="border-b h-20 flex items-center justify-center">
+      <PaddedWrapper className=" justify-between">
+        <Link href={"/"} className="font-sans font-semibold text-2xl">
+          SkillMatch
+        </Link>
+        <div className="flex items-center gap-4">
+          <NavigationMenu viewport={false}>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  asChild
+                  className={navigationMenuTriggerStyle()}
+                >
+                  <Link href="/">Home</Link>
                 </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="/companies?category=companies">
-                    <div className="font-medium">Companies</div>
-                    <div className="text-muted-foreground">
-                      Browse all companies.
-                    </div>
-                  </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Explore</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink asChild>
+                    <Link href="/explore?category=jobs" className="w-[200px]">
+                      <div className="font-medium">Jobs</div>
+                      <div className="text-muted-foreground">
+                        Browse all job listings.
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="/companies?category=companies">
+                      <div className="font-medium">Companies</div>
+                      <div className="text-muted-foreground">
+                        Browse all companies.
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  asChild
+                  className={navigationMenuTriggerStyle()}
+                >
+                  <Link href="/">Become an Employer</Link>
                 </NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={navigationMenuTriggerStyle()}
-              >
-                <Link href="/">Become an Employer</Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <JoinNowButton />
-      </div>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <JoinNowButton />
+        </div>
+      </PaddedWrapper>
     </header>
   );
 }
