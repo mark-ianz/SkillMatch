581c51ff2f2ce70261cc983fbb0f2a7e7f2cc4bd
mark-ianz <bustillomarkian23@gmail.com>
2025-10-06 03:41:06 +0800

    Refactor Layout component to include SessionProvider and improve structure

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import { ReactNode } from "react";
 import Header from "../_components/global/Header";
+import { SessionProvider } from "next-auth/react";
+import MainLayout from "../_components/layout/MainLayout";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <SessionProvider>
       <Header />
-      {children}
-    </>
+      <MainLayout className="items-center justify-center">{children}</MainLayout>
+    </SessionProvider>
   );
 }
