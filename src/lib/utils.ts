ac28f140e36f9029a53288cd7c89768bb85c03aa
mark-ianz <bustillomarkian23@gmail.com>
2025-10-07 02:36:40 +0800

    Aded a formatZodError utility for formatting zod error objects to array of string.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { ParsedError } from "@/app/types/zod.types";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import z, { ZodError } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatZodError(error: ZodError) {
+  const errors: ParsedError[] = JSON.parse(error.message);
+
+  return errors.map((err) => err.message);
 }
