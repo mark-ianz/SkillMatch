a8d0021779bd31b8a3d11d0b2cc64700ea43e0ff
mark-ianz <bustillomarkian23@gmail.com>
2025-10-07 04:33:24 +0800

    Implement GET endpoint for fetching onboarding data by ID

---
+++ src/app/api/onboarding/[id]/route.ts
@@ -0,0 +1,27 @@
+import { UserService } from "@/app/services/user.services";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  _req: Request,
+  context: { params: { id: string | undefined } }
+) {
+  const params = await context.params;
+
+  if (!params.id || isNaN(Number(params.id))) {
+    return NextResponse.json(
+      { error: "Invalid or missing ID parameter" },
+      { status: 400 }
+    );
+  }
+
+  const onboardingData = await UserService.getOnboarding(Number(params.id));
+
+  if (!onboardingData) {
+    return NextResponse.json(
+      { error: "Onboarding data not found" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(onboardingData, { status: 200 });
+}
