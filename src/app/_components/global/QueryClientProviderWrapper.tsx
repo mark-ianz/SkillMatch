ab66967c660cf9b1f34f9c3f863230c66e7e1fd3
mark-ianz <bustillomarkian23@gmail.com>
2025-10-07 04:31:48 +0800

    ^^

--- src/app/_components/global/QueryClientProviderWrapper.tsx
+++ src/app/_components/global/QueryClientProviderWrapper.tsx
@@ -1,4 +1,4 @@
-import { queryClient } from "@/lib/queryClient";
+import { queryClient } from "@/lib/queryClient.config";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 export default function QueryClientProviderWrapper({
