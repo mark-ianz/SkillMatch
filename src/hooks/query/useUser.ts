9cdf0c8daaa788ae49cd08562bf5147ce48fa2bb
mark-ianz <bustillomarkian23@gmail.com>
2025-10-09 00:41:33 +0800

    Add useUpdateStepOneOnboarding hook for step one mutation handling

--- src/hooks/query/useUser.ts
+++ src/hooks/query/useUser.ts
@@ -1,13 +1,32 @@
 import { OnboardingFullInfo } from "@/types/user.types";
 import { api } from "@/lib/axios";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { OnboardingStepOneSchema } from "@/schema/onboarding";
 
 export function useGetOnboarding(userId: number | undefined) {
   return useQuery({
     queryKey: ["onboarding", userId],
     queryFn: async () => {
-      const { data } = await api.get<OnboardingFullInfo>(`/onboarding/${userId}`);
+      const { data } = await api.get<OnboardingFullInfo>(
+        `/onboarding/${userId}`
+      );
       return data;
+    },
+  });
+}
+
+export function useUpdateStepOneOnboarding(
+  userId: number | undefined,
+  farthestStep: number
+) {
+  return useMutation({
+    mutationKey: ["onboarding", userId, "step-one"],
+    mutationFn: async (stepOneData: OnboardingStepOneSchema) => {
+      console.log("farthestStep", farthestStep);
+      await api.post(`/onboarding/${userId}/submit/${farthestStep}/step-one`, {
+        ...stepOneData,
+      });
+      return;
     },
   });
 }
