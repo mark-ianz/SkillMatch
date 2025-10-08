ae3cbf762501a481ef56c7ca46cb01b4690ecd26
mark-ianz <bustillomarkian23@gmail.com>
2025-10-08 17:19:17 +0800

    Add utility functions that capitalize first word.
    Add step details retrieval

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { ParsedError } from "@/app/types/zod.types";
+import { steps } from "@/const/steps";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import z, { ZodError } from "zod";
@@ -11,4 +12,12 @@ export function formatZodError(error: ZodError) {
   const errors: ParsedError[] = JSON.parse(error.message);
 
   return errors.map((err) => err.message);
+}
+
+export function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function getStepDetails(step: number) {
+  return steps.find((s) => s.step === step);
 }
