68f15395098aa6b69ab943e97679f238efe32d1d
mark-ianz <bustillomarkian23@gmail.com>
2025-10-12 13:58:54 +0800

    Enhance Label component to support required prop for indicating mandatory fields.

--- src/components/common/input/Label.tsx
+++ src/components/common/input/Label.tsx
@@ -1,18 +1,22 @@
 import React, { ReactNode } from "react";
-import { cn } from "@/lib/utils";;
+import { cn } from "@/lib/utils";
 
 type Props = {
   id: string | undefined;
   children: ReactNode;
   className?: string;
+  required?: boolean;
 };
 
-export default function Label({id, children, className}: Props) {
+export default function Label({
+  id,
+  children,
+  className,
+  required,
+}: Props) {
   return (
-    <label
-      htmlFor={id}
-      className={cn("text-sm font-medium", className)}
-    >
+    <label htmlFor={id} className={cn("text-sm font-medium", className)}>
+      {required && <span className="text-red-500 mr-1">*</span>}
       {children}
     </label>
   );
