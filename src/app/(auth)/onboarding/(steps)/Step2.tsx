0972740bb5ef675fd46a9b5d2bdcf24e14521c20
mark-ianz <bustillomarkian23@gmail.com>
2025-10-14 01:45:04 +0800

    Enhance Step 2 onboarding: add validation, error handling, and update user info submission

--- src/app/(auth)/onboarding/(steps)/Step2.tsx
+++ src/app/(auth)/onboarding/(steps)/Step2.tsx
@@ -6,19 +6,71 @@ import useSignupStore from "@/store/SignupStore";
 import { Button } from "@/components/ui/button";
 import SelectCityMunicipality from "../../../../components/common/input/SelectCityMunicipality";
 import SelectBarangay from "../../../../components/common/input/SelectBarangay";
+import { onboardingStepTwoSchema } from "@/schema/onboarding";
+import { useUpdateStepTwoOnboarding } from "@/hooks/query/useOnboarding";
+import { useSession } from "next-auth/react";
+import { ZodError } from "zod";
+import { formatZodError } from "@/lib/utils";
 
 export default function Step2() {
+  const session = useSession();
+
+  const farthestStep = useSignupStore((state) => state.farthestStep);
+
+  const { mutate } = useUpdateStepTwoOnboarding(
+    session.data?.user.user_id,
+    farthestStep
+  );
+
+  const house_number = useSignupStore((state) => state.house_number);
+  const subdivision = useSignupStore((state) => state.subdivision);
+  const postal_code = useSignupStore((state) => state.postal_code);
   const street_name = useSignupStore((state) => state.street_name);
 
   const setStreetName = useSignupStore((state) => state.setStreetName);
   const setHouseNumber = useSignupStore((state) => state.setHouseNumber);
   const setSubdivision = useSignupStore((state) => state.setSubdivision);
   const setPostalCode = useSignupStore((state) => state.setPostalCode);
+  const setError = useSignupStore((state) => state.setError);
 
+  // Submit handler
+  async function handleNextStep() {
+    // get all the data from Step 2 on the store
+    const house_number = useSignupStore.getState().house_number;
+    const subdivision = useSignupStore.getState().subdivision;
+    const postal_code = useSignupStore.getState().postal_code;
+    const city_municipality = useSignupStore.getState().city_municipality;
+    const barangay = useSignupStore.getState().barangay;
+
+    // validate the data
+    try {
+      // clear previous errors
+      setError(null);
+
+      const parsed = onboardingStepTwoSchema.parse({
+        house_number,
+        street_name,
+        subdivision,
+        postal_code,
+        city_municipality,
+        barangay,
+      });
+
+      // update the user info on the backend
+      mutate(parsed);
+    } catch (error) {
+      // catch the zod error
+      if (error instanceof ZodError) {
+        setError(formatZodError(error));
+        return;
+      }
+    }
+  }
   return (
     <StepContainer>
       <RowContainer>
         <InputWithLabel
+          value={house_number}
           required={true}
           placeholder="e.g., Blk 5 Lot 12 or 123"
           onChange={(e) => setHouseNumber(e.target.value)}
@@ -37,12 +89,14 @@ export default function Step2() {
 
       <RowContainer>
         <InputWithLabel
+          value={subdivision || ""}
           placeholder="e.g., Green Meadows Village (Optional)"
           onChange={(e) => setSubdivision(e.target.value)}
           label="Subdivision"
           containerClassName="w-full"
         />
         <InputWithLabel
+          value={postal_code}
           required={true}
           placeholder="e.g., 1101"
           onChange={(e) => setPostalCode(e.target.value)}
@@ -59,7 +113,7 @@ export default function Step2() {
         <SelectBarangay />
       </RowContainer>
 
-      <Button type="button" className="ml-auto w-24">
+      <Button onClick={handleNextStep} type="button" className="ml-auto w-24">
         Next
       </Button>
     </StepContainer>
