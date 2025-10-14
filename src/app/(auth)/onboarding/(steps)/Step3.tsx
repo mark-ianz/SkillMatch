d9f98e403d6e2b4d661d91649eba9c63dc0784c6
mark-ianz <bustillomarkian23@gmail.com>
2025-10-14 23:25:08 +0800

    Add Step 3 component for onboarding with college, course, year level, and expected graduation year selection

---
+++ src/app/(auth)/onboarding/(steps)/Step3.tsx
@@ -0,0 +1,112 @@
+import RowContainer from "@/components/common/input/RowContainer";
+import SelectWithLabel from "@/components/common/input/SelectWithLabel";
+import StepContainer from "@/components/page_specific/onboarding/StepContainer";
+import useSignupStore from "@/store/SignupStore";
+import React from "react";
+import college_courses from "@/data/college_courses.json";
+import { addYears } from "date-fns";
+import { Button } from "@/components/ui/button";
+
+export default function Step3() {
+  const college = useSignupStore((state) => state.college);
+  const course = useSignupStore((state) => state.course);
+  const year_level = useSignupStore((state) => state.year_level);
+  const expected_graduation_year = useSignupStore(
+    (state) => state.expected_graduation_year
+  );
+
+  const setCollege = useSignupStore((state) => state.setCollege);
+  const setCourse = useSignupStore((state) => state.setCourse);
+  const setYearLevel = useSignupStore((state) => state.setYearLevel);
+  const setExpectedGraduationYear = useSignupStore(
+    (state) => state.setExpectedGraduationYear
+  );
+
+  const college_list = Object.keys(college_courses).map((college) => ({
+    value: college,
+    label: college,
+  }));
+
+  const course_list = college
+    ? college_courses[college as keyof typeof college_courses].courses.map(
+        (course) => ({ value: course.abbr, label: course.course_name })
+      )
+    : [];
+  console.log(college_list);
+  console.log(course_list);
+  return (
+    <StepContainer>
+      <RowContainer>
+        <SelectWithLabel
+          required={true}
+          containerClassName="w-full"
+          id="college"
+          label="College"
+          value={college}
+          onChange={setCollege as (college: string) => void}
+          items={college_list}
+        />
+      </RowContainer>
+      <RowContainer>
+        <SelectWithLabel
+          disabled={!college}
+          required={true}
+          containerClassName="w-full"
+          id="course"
+          label="Course"
+          value={course}
+          onChange={setCourse as (course: string) => void}
+          items={course_list}
+        />
+      </RowContainer>
+      <RowContainer>
+        <SelectWithLabel
+          required={true}
+          containerClassName="w-full"
+          id="year_level"
+          label="Year Level"
+          value={year_level}
+          onChange={setYearLevel as (year_level: string) => void}
+          items={[
+            { value: "3rd year", label: "3rd year" },
+            { value: "4th year", label: "4th year" },
+          ]}
+        />
+        <SelectWithLabel
+          required={true}
+          containerClassName="w-full"
+          id="expected_graduation_year"
+          label="Expected Graduation Year"
+          value={expected_graduation_year}
+          onChange={
+            setExpectedGraduationYear as (
+              expected_graduation_year: string
+            ) => void
+          }
+          items={[
+            {
+              value: new Date().getFullYear().toString(),
+              label: new Date().getFullYear().toString(),
+            },
+            {
+              value: addYears(new Date(), 1).getFullYear().toString(),
+              label: addYears(new Date(), 1).getFullYear().toString(),
+            },
+            {
+              value: addYears(new Date(), 2).getFullYear().toString(),
+              label: addYears(new Date(), 2).getFullYear().toString(),
+            },
+          ]}
+        />
+      </RowContainer>
+      <Button
+        /* disabled={isPending}
+        onClick={handleNextStep} */
+        type="button"
+        className="ml-auto w-24"
+      >
+        Next
+      </Button>
+    </StepContainer>
+  );
+}
