577fd420f0765711f4a97b338803262d00fa1e27
mark-ianz <bustillomarkian23@gmail.com>
2025-10-16 22:32:40 +0800

    Created a POST route for updating user skills on the database.

---
+++ src/app/api/skills/update/[user_id]/route.ts
@@ -0,0 +1,30 @@
+import { formatZodError } from "@/lib/utils";
+import { updateSkillSchema } from "@/schema/skill";
+import { SkillServices } from "@/services/skill.services";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request, context: { params: { user_id: string } }) {
+  const params = await context.params;
+  const user_id = params.user_id;
+  if (!user_id) {
+    return new Response("Missing user_id", { status: 400 });
+  }
+
+  const { skills } = await request.json();
+
+  const { data, error, success } = updateSkillSchema.safeParse(skills);
+
+  if (!success) {
+    return NextResponse.json({ error: formatZodError(error), errorr: error }, { status: 422 });
+  }
+
+  try {
+    await SkillServices.updateSkills(user_id, data);
+    return NextResponse.json({ status: 204 });
+  } catch {
+    return NextResponse.json(
+      { error: "Failed to update skills" },
+      { status: 500 }
+    );
+  }
+}
