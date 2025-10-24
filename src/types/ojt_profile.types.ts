2cd45694c17a3624db5d724c98676cbcd6b9c927
mark-ianz <bustillomarkian23@gmail.com>
2025-10-24 23:14:36 +0800

    Refactor OJTProfile type to extend AcademicDetails and remove redundant fields

--- src/types/ojt_profile.types.ts
+++ src/types/ojt_profile.types.ts
@@ -1,10 +1,8 @@
-export type OJTProfile = {
+import { AcademicDetails } from "./user.types";
+
+export type OJTProfile = AcademicDetails & {
   ojt_id: number;
   user_id: number;
-  college: string;
-  course: string;
-  year_level: "3rd year" | "4th year";
-  expected_graduation_year: string;
   resume_path: string | null;
   visibility: "public" | "private";
   created_at: Date;
