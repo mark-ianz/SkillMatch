4190e74201ec6c73ead768425feebf278187162f
mark-ianz <bustillomarkian23@gmail.com>
2025-10-26 05:09:56 +0800

    Add useUpdateStepOneOnboardingCompany hook for onboarding step mutation

---
+++ src/hooks/query/useOnboardingCompany.ts
@@ -0,0 +1,23 @@
+import { api } from "@/lib/axios";
+import { EmployerOnboardingStepOneSchema } from "@/schema/onboarding";
+import useOnboardingStore from "@/store/OnboardingStore";
+import { useMutation } from "@tanstack/react-query";
+
+const nextStep = useOnboardingStore.getState().nextStep;
+const farthestStep = useOnboardingStore.getState().farthestStep;
+
+export function useUpdateStepOneOnboardingCompany(company_id: number | undefined) {
+  return useMutation({
+    mutationKey: ["onboarding", company_id, "step-one"],
+    mutationFn: async (stepOneData: EmployerOnboardingStepOneSchema) => {
+      await api.post(`/onboarding/${company_id}/submit/${farthestStep}/step-one/company`, {
+        ...stepOneData,
+      });
+      return;
+    },
+    onSuccess: () => {
+      // increment the step on the store
+      nextStep();
+    },
+  });
+}
