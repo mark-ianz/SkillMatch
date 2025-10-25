09ec4185098d5d4936fe4ed9df3e78828d11d6b7
mark-ianz <bustillomarkian23@gmail.com>
2025-10-25 20:07:51 +0800

    Add setter methods for each input on company details in CompanyStore

--- src/store/CompanyStore.ts
+++ src/store/CompanyStore.ts
@@ -3,6 +3,12 @@ import { create } from "zustand";
 
 export type CompanyStoreState = Partial<Company> & {
   setCompany: (data: Partial<Company>) => void;
+  setCompanyName: (company_name: string) => void;
+  setCompanyEmail: (company_email: string) => void;
+  setTelephoneNumber: (telephone_number: string) => void;
+  setPhoneNumber: (phone_number: string) => void;
+  setWebsite: (website: string) => void;
+  setFacebookPage: (facebook_page: string) => void;
 };
 
 const useCompanyStore = create<CompanyStoreState>((set) => ({
@@ -13,6 +19,12 @@ const useCompanyStore = create<CompanyStoreState>((set) => ({
   website: "",
   facebook_page: "",
   setCompany: (data) => set(data),
+  setCompanyName: (company_name) => set({ company_name }),
+  setCompanyEmail: (company_email) => set({ company_email }),
+  setTelephoneNumber: (telephone_number) => set({ telephone_number }),
+  setPhoneNumber: (phone_number) => set({ phone_number }),
+  setWebsite: (website) => set({ website }),
+  setFacebookPage: (facebook_page) => set({ facebook_page }),
   clearEmployer: () =>
     set({
       company_name: "",
