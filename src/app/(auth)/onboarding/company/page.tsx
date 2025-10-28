86948a163444d0991d2b819924c82f93ed6e8ea0
mark-ianz <bustillomarkian23@gmail.com>
2025-10-29 02:15:33 +0800

    Add Step2 and Step3 components to onboarding form inputs

--- src/app/(auth)/onboarding/company/page.tsx
+++ src/app/(auth)/onboarding/company/page.tsx
@@ -3,9 +3,17 @@
 import React from "react";
 import Step1 from "./(steps)/Step1";
 import useOnboardingStore from "@/store/OnboardingStore";
+import Step2 from "./(steps)/Step2";
+import Step3 from "./(steps)/Step3";
 
 export default function FormInputs() {
   const currentStep = useOnboardingStore((state) => state.currentStep);
 
-  return <>{currentStep === 1 && <Step1 />}</>;
+  return (
+    <>
+      {currentStep === 1 && <Step1 />}
+      {currentStep === 2 && <Step2 />}
+      {currentStep === 3 && <Step3 />}
+    </>
+  );
 }
