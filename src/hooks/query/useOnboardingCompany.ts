34a386300b81d6523af5c974f6c5c9e4a533b53e
mark-ianz <bustillomarkian23@gmail.com>
2025-10-29 02:14:34 +0800

    Add step two schema import and mutation for onboarding company

--- src/hooks/query/useOnboardingCompany.ts
+++ src/hooks/query/useOnboardingCompany.ts
@@ -1,5 +1,5 @@
 import { api } from "@/lib/axios";
-import { EmployerOnboardingStepOneSchema } from "@/schema/onboarding";
+import { EmployerOnboardingStepOneSchema, EmployerOnboardingStepTwoSchema } from "@/schema/onboarding";
 import useOnboardingStore from "@/store/OnboardingStore";
 import { useMutation } from "@tanstack/react-query";
 
@@ -12,6 +12,22 @@ export function useUpdateStepOneOnboardingCompany(company_id: number | undefined
     mutationFn: async (stepOneData: EmployerOnboardingStepOneSchema) => {
       await api.post(`/onboarding/${company_id}/submit/${farthestStep}/step-one/company`, {
         ...stepOneData,
+      });
+      return;
+    },
+    onSuccess: () => {
+      // increment the step on the store
+      nextStep();
+    },
+  });
+}
+
+export function useUpdateStepTwoOnboardingCompany(company_id: number | undefined) {
+  return useMutation({
+    mutationKey: ["onboarding", company_id, "step-two"],
+    mutationFn: async (stepTwoData: EmployerOnboardingStepTwoSchema) => {
+      await api.post(`/onboarding/${company_id}/submit/${farthestStep}/step-two/company`, {
+        ...stepTwoData,
       });
       return;
     },
