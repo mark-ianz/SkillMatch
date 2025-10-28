f78be1aaa440c945458d1d79fb866ce55e111a07
mark-ianz <bustillomarkian23@gmail.com>
2025-10-29 03:18:05 +0800

    Refactor useRequireCompany hook to improve authentication handling and redirect logic

--- src/hooks/useRequireCompany.ts
+++ src/hooks/useRequireCompany.ts
@@ -1,12 +1,16 @@
 import { useSession } from "next-auth/react";
-import { unauthorized } from "next/navigation";
+import { forbidden, unauthorized } from "next/navigation";
 
 export default function useRequireCompany(): number {
   const session = useSession();
 
+  if (session.status === "unauthenticated") {
+    unauthorized();
+  }
+
   if (session.status === "authenticated" && !session.data?.user.company_id) {
     // Redirect users that are authenticated but are not associated with a company
-    unauthorized();
+    forbidden();
   }
 
   const company_id = session.data?.user.company_id as number;
