046774ebdd7dc6845196f176039f6584a6ddf7f2
mark-ianz <bustillomarkian23@gmail.com>
2025-10-29 03:18:17 +0800

    Add session validation to FormInputs component for unauthorized access handling

--- src/app/(auth)/onboarding/company/page.tsx
+++ src/app/(auth)/onboarding/company/page.tsx
@@ -5,8 +5,16 @@ import Step1 from "./(steps)/Step1";
 import useOnboardingStore from "@/store/OnboardingStore";
 import Step2 from "./(steps)/Step2";
 import Step3 from "./(steps)/Step3";
+import { useSession } from "next-auth/react";
+import { unauthorized } from "next/navigation";
 
 export default function FormInputs() {
+  const session = useSession();
+
+  if (!session.data?.user.company_id && session.status === "unauthenticated") {
+    unauthorized();
+  }
+
   const currentStep = useOnboardingStore((state) => state.currentStep);
 
   return (
