67e5ab624ea32a66a7eda186be65b6b93d4d6aa8
mark-ianz <bustillomarkian23@gmail.com>
2025-11-02 19:52:16 +0800

    Refactor JoinNowButton to include pathname-based variant logic for improved button styling

--- src/components/common/button/JoinNowButton.tsx
+++ src/components/common/button/JoinNowButton.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 export default function JoinNowButton() {
   const router = useRouter();
+  const path = usePathname();
 
   const handleJoinNow = () => {
     router.push("/signin");
   };
 
-  return <Button onClick={handleJoinNow}>Join Now</Button>;
+  return (
+    <Button
+      variant={path.includes("ojt") ? "default" : "default_employer"}
+      onClick={handleJoinNow}
+    >
+      Join Now
+    </Button>
+  );
 }
