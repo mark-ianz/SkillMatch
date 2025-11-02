28481b7a1b9d88478df1e4b1fe61a867e36ec5fd
mark-ianz <bustillomarkian23@gmail.com>
2025-11-02 19:51:32 +0800

    Enhance LoadingGeneric component to support full-screen loading state

--- src/components/global/LoadingGeneric.tsx
+++ src/components/global/LoadingGeneric.tsx
@@ -1,7 +1,28 @@
 import { Loader2Icon } from "lucide-react";
 import React from "react";
-import { cn } from "@/lib/utils";;
+import { cn } from "@/lib/utils";
 
-export default function LoadingGeneric({ className }: { className?: string }) {
-  return <Loader2Icon className={cn("animate-spin w-12 h-12 text-skillmatch-muted-light", className)} />;
+export default function LoadingGeneric({
+  className,
+  full,
+}: {
+  className?: string;
+  full?: boolean;
+}) {
+  if (full) {
+    return (
+      <div className="flex items-center justify-center h-[100px]">
+        <LoadingGeneric className="w-6 h-6" />
+      </div>
+    );
+  }
+
+  return (
+    <Loader2Icon
+      className={cn(
+        "animate-spin w-12 h-12 text-skillmatch-muted-light",
+        className
+      )}
+    />
+  );
 }
