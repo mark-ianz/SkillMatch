3a9c6f777911aacf628f6cf858bf5301643b5814
mark-ianz <bustillomarkian23@gmail.com>
2025-11-05 05:38:58 +0800

    Update MainLayout to use min-height for better layout responsiveness

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({
   className?: string;
 }) {
   return (
-    <main className={cn(className, "h-[calc(100dvh-80px)] flex flex-col")}>
+    <main className={cn(className, "min-h-[calc(100dvh-80px)] flex flex-col")}>
       <PaddedWrapper className="flex items-center justify-center py-20">
         {children}
       </PaddedWrapper>
