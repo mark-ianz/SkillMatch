cd409c7ad20b5b3fdebe58e63c25069cbcd073a2
mark-ianz <bustillomarkian23@gmail.com>
2025-11-05 05:39:07 +0800

    Refactor InputWithLabel component to support both input and textarea types

--- src/components/common/input/InputWithLabel.tsx
+++ src/components/common/input/InputWithLabel.tsx
@@ -1,34 +1,66 @@
 import React, { forwardRef } from "react";
 import { Input } from "@/components/ui/input";
-import { cn } from "@/lib/utils";;
+import { cn } from "@/lib/utils";
 import Label from "./Label";
+import { Textarea } from "@/components/ui/textarea";
 
-interface InputWithLabelProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+type CommonProps = {
   label: string;
   containerClassName?: string;
   labelClassName?: string;
   inputClassName?: string;
   required?: boolean;
   optional?: boolean;
-}
+  as?: "input" | "textarea";
+};
 
-const InputWithLabel = forwardRef<HTMLInputElement, InputWithLabelProps>(
+type InputWithLabelProps =
+  | (CommonProps & React.InputHTMLAttributes<HTMLInputElement>)
+  | (CommonProps & React.TextareaHTMLAttributes<HTMLTextAreaElement>);
+
+const InputWithLabel = forwardRef<
+  HTMLInputElement | HTMLTextAreaElement,
+  InputWithLabelProps
+>(
   (
-    { label, id, containerClassName, labelClassName, inputClassName, required, optional, ...props },
+    {
+      label,
+      id,
+      containerClassName,
+      labelClassName,
+      inputClassName,
+      required,
+      optional,
+      as = "input",
+      ...props
+    },
     ref
   ) => {
     return (
       <div className={cn("flex flex-col gap-1", containerClassName)}>
-        <Label id={id} className={labelClassName} required={required} optional={optional}>
+        <Label
+          id={id}
+          className={labelClassName}
+          required={required}
+          optional={optional}
+        >
           {label}
         </Label>
-        <Input
-          id={id}
-          ref={ref}
-          className={cn(inputClassName)}
-          {...props}
-        />
+        {as === "textarea" ? (
+          <Textarea
+            id={id}
+            ref={ref as React.Ref<HTMLTextAreaElement>}
+            className={cn(inputClassName)}
+            {...(props as React.TextareaHTMLAttributes<HTMLTextAreaElement>)}
+          />
+        ) : (
+          <Input
+            id={id}
+            ref={ref as React.Ref<HTMLInputElement>}
+            className={cn(inputClassName)}
+            {...(props as React.InputHTMLAttributes<HTMLInputElement>)}
+          />
+        )}
       </div>
     );
   }
