408033864fd368b7e37e2d697571483393217259
mark-ianz <bustillomarkian23@gmail.com>
2025-11-05 05:40:01 +0800

    Add JobPostingStore for managing job posting state and form data

---
+++ src/store/JobPostingStore.ts
@@ -0,0 +1,41 @@
+import { JobPost } from "@/types/job_post.types";
+import { create } from "zustand";
+
+interface JobPostingState {
+  formData: Partial<JobPost>;
+  setFormData: (data: Partial<JobPost>) => void;
+  updateField: <K extends keyof JobPost>(field: K, value: JobPost[K]) => void;
+  reset: () => void;
+}
+
+const initialState: Partial<JobPost> = {
+  job_title: "",
+  program_required: [],
+  job_category: [],
+  available_positions: 1,
+  job_overview: "",
+  job_responsibilities: [],
+  preferred_qualifications: "",
+  work_arrangement: "On-site",
+  is_paid: false,
+  allowance_description: null,
+  soft_skills: [],
+  technical_skills: [],
+  street_name: "",
+  barangay: "",
+  city_municipality: "",
+  postal_code: "",
+};
+
+export const useJobPostingStore = create<JobPostingState>((set) => ({
+  formData: initialState,
+  setFormData: (data) =>
+    set((state) => ({
+      formData: { ...state.formData, ...data },
+    })),
+  updateField: (field, value) =>
+    set((state) => ({
+      formData: { ...state.formData, [field]: value },
+    })),
+  reset: () => set({ formData: initialState }),
+}));
