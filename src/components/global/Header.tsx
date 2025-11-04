3a16b3761da22247b8e7091d5b7a4ea9c17b1111
mark-ianz <bustillomarkian23@gmail.com>
2025-11-05 05:38:33 +0800

    Replace SkillMatch link with DynamicLogo component in Header

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -11,14 +11,13 @@ import Link from "next/link";
 import React from "react";
 import JoinNowButton from "../common/button/JoinNowButton";
 import PaddedWrapper from "./PaddedWrapper";
+import DynamicLogo from "./DynamicLogo";
 
 export default function Header() {
   return (
     <header className="border-b h-20 flex items-center justify-center sticky top-0 bg-white z-50">
       <PaddedWrapper className=" justify-between">
-        <Link href={"/"} className="font-sans font-semibold text-2xl">
-          SkillMatch
-        </Link>
+        <DynamicLogo />
         <div className="flex items-center gap-4">
           <NavigationMenu viewport={false}>
             <NavigationMenuList>
