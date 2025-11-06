f5be1cf4c067cd59aea699df6c690971240fd22b
mark-ianz <bustillomarkian23@gmail.com>
2025-11-06 23:49:04 +0800

    Refactor SelectCityMunicipality component to accept props for value and onValueChange, removing direct store access to make it reusable.

--- src/components/common/input/SelectCityMunicipality.tsx
+++ src/components/common/input/SelectCityMunicipality.tsx
@@ -1,32 +1,29 @@
 import React from "react";
 import city_municipality from "@/data/city_municipality.json";
 import ComboBoxWithLabel from "@/components/common/input/ComboBoxWithLabel";
-import useUserStore from "@/store/UserStore";
 
-export default function SelectCityMunicipality() {
+export default function SelectCityMunicipality({
+  value,
+  onValueChange,
+} : {
+  value: string | null | undefined;
+  onValueChange: (value: string) => void;
+}) {
   const city_municipality_list = Object.keys(city_municipality).map((key) => ({
     label: key,
     value: key,
   }));
 
-  const selected_city_municipality = useUserStore(
-    (state) => state.city_municipality
-  );
-
-  const setCityMunicipality = useUserStore(
-    (state) => state.setCityMunicipality
-  );
-
   return (
     <ComboBoxWithLabel
       required={true}
       containerClassName="w-full"
-      value={selected_city_municipality || ""}
+      value={value || ""}
       items={city_municipality_list}
       searchFor="city/municipality"
       id="city-municipality"
       label="City/Municipality"
-      onValueChange={setCityMunicipality}
+      onValueChange={onValueChange}
     />
   );
 }
