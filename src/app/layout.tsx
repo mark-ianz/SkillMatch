6c404bce7ebad461bc74f746959147c39f1ebaa6
mark-ianz <bustillomarkian23@gmail.com>
2025-11-09 06:26:23 +0800

    Add font weight definitions for Geist and Geist_Mono fonts in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,13 @@ import { Toaster } from "@/components/ui/sonner";
 const geistSans = Geist({
   variable: "--font-geist",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const sansation = Sansation({
