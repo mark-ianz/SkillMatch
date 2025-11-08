e81fef41cb4bb34aa3436c3c7172ae448f172c09
mark-ianz <bustillomarkian23@gmail.com>
2025-11-08 23:31:02 +0800

    Pass wrapperClassName prop to PaddedWrapper in MainLayout for improved styling flexibility

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,13 +5,15 @@ import PaddedWrapper from "../global/PaddedWrapper";
 export default function MainLayout({
   children,
   className,
+  wrapperClassName,
 }: {
   children: ReactNode;
   className?: string;
+  wrapperClassName?: string;
 }) {
   return (
     <main className={cn(className, "min-h-[calc(100dvh-80px)] flex flex-col")}>
-      <PaddedWrapper className="flex items-center justify-center py-20">
+      <PaddedWrapper className={cn("flex items-center justify-center py-20", wrapperClassName)}>
         {children}
       </PaddedWrapper>
     </main>
