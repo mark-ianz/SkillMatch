66b3196811f1a481f77bc6810af26a867f66a4ca
mark-ianz <bustillomarkian23@gmail.com>
2025-11-10 06:01:06 +0800

    Update Company type to allow null values for industry and description fields

--- src/types/company.types.ts
+++ src/types/company.types.ts
@@ -3,8 +3,8 @@ import { Address } from "./user.types";
 
 export type Company = EmployerOnboardingStepOneSchema & {
   company_id: number;
-  industry: string[];
-  description: string;
+  industry: string[] | null;
+  description: string | null;
   company_image: string | null | undefined;
   mou_path: string;
   loi_path: string;
