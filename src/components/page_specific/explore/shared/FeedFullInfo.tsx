ca4180a055ff2b4d2da374092e6723c6fe2bfd12
mark-ianz <bustillomarkian23@gmail.com>
2025-11-10 16:24:28 +0800

    Clear whitespace and removed empty curly braces.

--- src/components/page_specific/explore/shared/FeedFullInfo.tsx
+++ src/components/page_specific/explore/shared/FeedFullInfo.tsx
@@ -3,21 +3,17 @@ import { JobPostFullInfo } from "../../job_post/JobPostFullInfo";
 import { useFeedStore } from "@/store/FeedStore";
 import { CompanyProfile } from "../company/CompanyProfile";
 
-export default function FeedFullInfo({}) {
+export default function FeedFullInfo() {
   const exploreType = useFeedStore((state) => state.exploreType);
 
   return (
     <div className="flex-4 sticky top-28 bottom-4 pb-20 h-fit">
       <div className="max-h-[calc(100vh-9rem)] overflow-y-auto border pb-4 rounded-lg">
         {exploreType === "job-posts" && (
-          <JobPostFullInfo
-            className="border-0 shadow-none"
-          />
+          <JobPostFullInfo className="border-0 shadow-none" />
         )}
         {exploreType === "companies" && (
-          <CompanyProfile
-            className="border-0 shadow-none"
-          />
+          <CompanyProfile className="border-0 shadow-none" />
         )}
       </div>
     </div>
