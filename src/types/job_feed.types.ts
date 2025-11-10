5cf5b1a56b86d081bf5228313b919b55d520665d
mark-ianz <bustillomarkian23@gmail.com>
2025-11-11 02:10:02 +0800

    Add industries and jobCategories to JobFeedFilters type

--- src/types/job_feed.types.ts
+++ src/types/job_feed.types.ts
@@ -2,6 +2,8 @@ export type JobFeedFilters = {
   courses: string[];
   locations: string[];
   workArrangement: string[];
+  industries: string[];
+  jobCategories: string[];
   isPaid?: boolean;
 };
 
