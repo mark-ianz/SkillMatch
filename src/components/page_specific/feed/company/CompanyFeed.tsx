bcdc84bf12315cbfe5bd21f938c62c487d2f18c2
mark-ianz <bustillomarkian23@gmail.com>
2025-11-10 08:11:22 +0800

    Add CompanyFeed component to fetch and display a list of companies with loading and error handling

---
+++ src/components/page_specific/feed/company/CompanyFeed.tsx
@@ -0,0 +1,64 @@
+import { CompanyProfile } from "@/types/company.types";
+import React, { useEffect } from "react";
+import { CompanyPreview } from "./CompanyPreview";
+import { useCompanies } from "@/hooks/query/useCompanies";
+import LoadingGeneric from "@/components/global/LoadingGeneric";
+import { useFeedStore } from "@/store/FeedStore";
+
+export default function CompanyFeed() {
+  const { data: companies, isLoading, error } = useCompanies();
+  const selected_company = useFeedStore((state) => state.selected_company);
+  const setSelectedCompany = useFeedStore((state) => state.setSelectedCompany);
+
+  console.log(companies)
+  //Set initial selected item when data loads
+  useEffect(() => {
+    if (
+      companies &&
+      companies.length > 0 &&
+      selected_company === null
+    ) {
+      setSelectedCompany(companies[0]);
+    }
+  }, [companies, selected_company, setSelectedCompany]);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center">
+        <LoadingGeneric />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center">
+        <p className="text-destructive">
+          Failed to load companies. Please try again later.
+        </p>
+      </div>
+    );
+  }
+
+  if (!companies && !isLoading) {
+    return (
+      <div className="flex items-center justify-center">
+        <p className="text-muted">No companies available</p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {companies!.map((company: CompanyProfile) => (
+        <div
+          key={company.company_id}
+          onClick={() => setSelectedCompany(company)}
+          className={`cursor-pointer transition-all`}
+        >
+          <CompanyPreview company={company} />
+        </div>
+      ))}
+    </>
+  );
+}
