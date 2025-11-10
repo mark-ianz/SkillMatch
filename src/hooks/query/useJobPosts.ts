dfc31afb151e44941e71996bd860e7fbc2f9f653
mark-ianz <bustillomarkian23@gmail.com>
2025-11-11 03:45:09 +0800

    Enhance useJobPosts hook to support optional filters for querying job posts

--- src/hooks/query/useJobPosts.ts
+++ src/hooks/query/useJobPosts.ts
@@ -3,13 +3,30 @@ import { api } from "@/lib/axios";
 import { JobPostingFormData } from "@/schema/job-posting.schema";
 import { toast } from "sonner";
 import { JobPost } from "@/types/job_post.types";
+import { JobFeedFilters } from "@/types/job_feed.types";
 
-// Query: Get all job posts
-export function useJobPosts() {
+// Query: Get all job posts with optional filters
+export function useJobPosts(filters?: JobFeedFilters) {
   return useQuery({
-    queryKey: ["job-posts"],
+    queryKey: ["job-posts", filters],
     queryFn: async () => {
-      const { data } = await api.get("/company/job-post/all");
+      const params = new URLSearchParams();
+      
+      if (filters) {
+        filters.courses?.forEach((c) => params.append("course", c));
+        filters.locations?.forEach((l) => params.append("location", l));
+        filters.workArrangement?.forEach((w) => params.append("arrangement", w));
+        filters.industries?.forEach((i) => params.append("industry", i));
+        filters.jobCategories?.forEach((j) => params.append("jobCategory", j));
+        if (filters.isPaid !== undefined) {
+          params.append("paid", String(filters.isPaid));
+        }
+      }
+
+      const queryString = params.toString();
+      const url = queryString ? `/feed?${queryString}` : "/feed";
+      
+      const { data } = await api.get(url);
       return data.job_posts as JobPost[];
     },
     staleTime: 1000 * 60 * 5, // 5 minutes
