62669a29c7f656a661066c21d5c6daa60f603fa3
mark-ianz <bustillomarkian23@gmail.com>
2025-11-11 20:53:28 +0800

    Refactor FeedFullInfo to handle cases with no selected job post or company and enforce non-null assertions for selected values

--- src/components/page_specific/explore/shared/FeedFullInfo.tsx
+++ src/components/page_specific/explore/shared/FeedFullInfo.tsx
@@ -9,18 +9,34 @@ export default function FeedFullInfo() {
   const selected_job_post = useFeedStore((state) => state.selected_job_post);
   const selected_company = useFeedStore((state) => state.selected_company);
 
+  if (!selected_job_post && exploreType === "job-posts") {
+    return (
+      <div className="flex items-center justify-center">
+        <p className="text-muted">No job post selected</p>
+      </div>
+    );
+  }
+
+  if (!selected_company && exploreType === "companies") {
+    return (
+      <div className="flex items-center justify-center">
+        <p className="text-muted">No company selected</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-4 sticky top-28 bottom-4 pb-20 h-fit">
       <div className="max-h-[calc(100vh-9rem)] overflow-y-auto border pb-4 rounded-lg">
         {exploreType === "job-posts" && (
           <JobPostFullInfo
-            job={selected_job_post}
+            job={selected_job_post!}
             className="border-0 shadow-none"
           />
         )}
         {exploreType === "companies" && (
           <CompanyProfile
-            company={selected_company}
+            company={selected_company!}
             className="border-0 shadow-none"
           />
         )}
