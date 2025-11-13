7c78183c7e0d161f7234417caadf55037d29a5de
mark-ianz <bustillomarkian23@gmail.com>
2025-11-14 04:53:51 +0800

    Remove SessionProvider from Layout components in auth and site

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,11 @@
-"use client";
-
 import { ReactNode } from "react";
 import Header from "../../components/global/Header";
-import { SessionProvider } from "next-auth/react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <SessionProvider>
+    <>
       <Header />
       {children}
-    </SessionProvider>
+    </>
   );
 }
--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,16 +1,11 @@
-"use client";
-
 import { ReactNode } from "react";
 import Footer from "../../components/global/Footer";
 import Header from "../../components/global/Header";
-import { SessionProvider } from "next-auth/react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <SessionProvider>
-        <Header />
-      </SessionProvider>
+      <Header />
       {children}
       <Footer />
     </>
