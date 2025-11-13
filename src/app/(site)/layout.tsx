0daea258015666d20b419bca37ea75cac62f75b6
mark-ianz <bustillomarkian23@gmail.com>
2025-11-14 01:48:32 +0800

    Wrap Header component with SessionProvider for session management

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import { ReactNode } from "react";
 import Footer from "../../components/global/Footer";
 import Header from "../../components/global/Header";
+import { SessionProvider } from "next-auth/react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Header />
+      <SessionProvider>
+        <Header />
+      </SessionProvider>
       {children}
       <Footer />
     </>
