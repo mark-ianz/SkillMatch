c2a00c56366a084c8638d3fd4af4fa87de51f687
mark-ianz <bustillomarkian23@gmail.com>
2025-11-15 02:33:22 +0800

    Integrate SessionSync component within SessionProviderWrapper for session synchronization

--- src/components/providers/SessionProviderWrapper.tsx
+++ src/components/providers/SessionProviderWrapper.tsx
@@ -1,10 +1,16 @@
 import { SessionProvider } from "next-auth/react";
 import React from "react";
+import { SessionSync } from "../auth/SessionSync";
 
 export default function SessionProviderWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      {children}
+      <SessionSync />
+    </SessionProvider>
+  );
 }
