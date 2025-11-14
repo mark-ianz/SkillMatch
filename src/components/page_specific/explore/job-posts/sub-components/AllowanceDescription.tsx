41193cb9d6508926845faea2cce7a111e666031a
mark-ianz <bustillomarkian23@gmail.com>
2025-11-15 02:57:25 +0800

    Refactor AllowanceDescription component to accept className prop for better styling flexibility

--- src/components/page_specific/explore/job-posts/sub-components/AllowanceDescription.tsx
+++ src/components/page_specific/explore/job-posts/sub-components/AllowanceDescription.tsx
@@ -1,15 +1,18 @@
 import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 import React from "react";
 
 export default function AllowanceDescription({
   allowance_description,
+  className
 }: {
   allowance_description: string | null | undefined;
+  className?: string;
 }) {
   return (
     <>
       {allowance_description && (
-        <Badge variant="secondary" className="text-xs font-medium mt-2">
+        <Badge variant="secondary" className={cn("text-xs font-medium", className)}>
           {allowance_description}
         </Badge>
       )}
