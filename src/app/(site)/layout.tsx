60ccf88ee3417332292214bcc7c14a4bccea649a
mark-ianz <bustillomarkian23@gmail.com>
2025-11-17 23:40:08 +0800

    Fix import path for Header component and uncomment Header and Footer in Layout

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,13 +1,13 @@
 import { ReactNode } from "react";
 import Footer from "../../components/global/Footer";
-import Header from "../../components/global/Header";
+import Header from "../../components/global/header/Header";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       {children}
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
