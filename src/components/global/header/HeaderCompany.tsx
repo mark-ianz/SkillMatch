408aa5688607aa45a2a68f6552248db229faf02c
mark-ianz <bustillomarkian23@gmail.com>
2025-11-18 05:08:43 +0800

    Rename HeaderOJT to HeaderCompany and update DynamicLogo role to Company

--- src/components/global/header/HeaderCompany.tsx
+++ src/components/global/header/HeaderCompany.tsx
@@ -5,12 +5,12 @@ import DynamicLogo from "../DynamicLogo";
 import NavigationMenuCompany from "./NavigationMenuCompany";
 import Link from "next/link";
 
-export default async function HeaderOJT() {
+export default async function HeaderCompany() {
   return (
     <header className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50">
       <div className="container mx-auto px-4 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <DynamicLogo role="OJT" />
+          <DynamicLogo role="Company" />
           <div className="flex items-center gap-4">
             <NavigationMenuCompany />
             <Button asChild variant="outline" size="sm">
