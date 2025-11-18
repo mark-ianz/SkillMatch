07d0c85074d4421d34a1dfe9829ba14844abbf1b
mark-ianz <bustillomarkian23@gmail.com>
2025-11-18 22:09:57 +0800

    Add HeaderActions component for user session management and notifications

---
+++ src/components/global/header/HeaderActions.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Button } from "../../ui/button";
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/lib/auth";
+import { getRoleName } from "@/lib/utils";
+import { Notification } from "@/types/notification.types";
+import NotificationPopover from "./NotificationPopover";
+import ProfilePopover from "./ProfilePopover";
+
+export default async function HeaderActions({
+  notifications,
+}: {
+  notifications?: Notification[];
+}) {
+  const session = await getServerSession(authConfig);
+  const role = getRoleName(session?.user.role_id);
+
+  if (!session) {
+    return (
+      <div className="flex gap-2">
+        <Button asChild variant="outline" size="sm">
+          <Link href="/signin">Sign In</Link>
+        </Button>
+        <Button
+          asChild
+          size="sm"
+          className="bg-skillmatch-primary-green text-skillmatch-light hover:bg-skillmatch-primary-green/90"
+        >
+          <Link href="/signup">Get Started</Link>
+        </Button>
+      </div>
+    );
+  }
+
+  if (role === "OJT" || role === "Company") {
+    return (
+      <div className="flex items-center gap-2">
+        <NotificationPopover notifications={notifications || []} />
+        <ProfilePopover
+          email={session.user.email || ""}
+          name={session.user.name || ""}
+          userType={role}
+          profile_image={session.user.image}
+        />
+      </div>
+    );
+  }
+}
