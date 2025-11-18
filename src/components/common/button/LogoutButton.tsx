7358c3a7b91ff654bf18bd09169dadb5c46227c7
mark-ianz <bustillomarkian23@gmail.com>
2025-11-18 21:55:50 +0800

    Refactor LogoutButton component to include className prop and enhance styling

--- src/components/common/button/LogoutButton.tsx
+++ src/components/common/button/LogoutButton.tsx
@@ -1,9 +1,21 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { LogOut } from "lucide-react";
 import { signOut } from "next-auth/react";
 import React from "react";
 
-export default function LogoutButton() {
-  return <Button onClick={() => signOut({callbackUrl: "/"})}>Logout</Button>;
+export default function LogoutButton({className}: {className?: string}) {
+  return (
+    <Button
+      onClick={() => signOut({ callbackUrl: "/" })}
+      variant="ghost"
+      className={cn("w-full justify-start text-red-600 hover:text-red-600 hover:bg-red-50", className)}
+      size="sm"
+    >
+      <LogOut className="mr-2 h-4 w-4" />
+      Log Out
+    </Button>
+  );
 }
