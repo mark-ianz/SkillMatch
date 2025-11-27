7ef90912896ce18e6085db27fb658ebd1d3e462c
mark-ianz <bustillomarkian23@gmail.com>
2025-11-28 02:05:43 +0800

    refactor: enhance Explore component to sync store with URL parameters

--- src/components/page_specific/explore/shared/Explore.tsx
+++ src/components/page_specific/explore/shared/Explore.tsx
@@ -1,13 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
 import { JobExploreHeader } from "../job-posts/JobExploreHeader";
 import CompanyExplore from "../company/CompanyExplore";
 import { useExploreStore } from "@/store/ExploreStore";
 import JobPostExplore from "../job-posts/JobPostExplore";
+import { ExploreType } from "@/types/job_explore.types";
 
 export default function Explore({}) {
+  const params = useParams();
   const exploreType = useExploreStore((state) => state.exploreType);
+  const setExploreType = useExploreStore((state) => state.setExploreType);
+
+  // Sync store with URL parameter
+  useEffect(() => {
+    const urlExploreType = params.exploreType as ExploreType;
+    if (urlExploreType && urlExploreType !== exploreType) {
+      setExploreType(urlExploreType);
+    }
+  }, [params.exploreType, exploreType, setExploreType]);
 
   return (
     <div className="flex-3 flex flex-col space-y-4">
