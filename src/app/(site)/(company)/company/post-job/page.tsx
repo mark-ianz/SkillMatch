878f956640ded3631bf368c713951263aa2d0164
mark-ianz <bustillomarkian23@gmail.com>
2025-11-28 01:37:24 +0800

    refactor: enhance layout structure for Post Job page to improve styling consistency

--- src/app/(site)/(company)/company/post-job/page.tsx
+++ src/app/(site)/(company)/company/post-job/page.tsx
@@ -7,8 +7,8 @@ import { SessionProvider } from "next-auth/react";
 
 export default function PostJobPage() {
   return (
-    <MainLayout className="items-center">
-      <div className="flex flex-col w-full max-w-3xl">
+    <MainLayout className="items-center w-full" wrapperClassName="w-full py-0">
+      <div className="flex flex-col container max-w-3xl">
         <div className="mb-8">
           <h1 className="text-3xl font-bold">Post a Job</h1>
           <p className="text-muted-foreground mt-2">
