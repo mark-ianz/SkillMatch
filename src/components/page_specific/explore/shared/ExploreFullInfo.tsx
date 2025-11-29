d0401ff48d523d35bb382bb32a862b7d9e5fd425
mark-ianz <bustillomarkian23@gmail.com>
2025-11-30 04:42:58 +0800

    feat(explore-full-info): refactor session management to use next-auth and simplify loading state

--- src/components/page_specific/explore/shared/ExploreFullInfo.tsx
+++ src/components/page_specific/explore/shared/ExploreFullInfo.tsx
@@ -5,17 +5,21 @@ import { JobPostFullInfo } from "../../job_post/JobPostFullInfo";
 import { useExploreStore } from "@/store/ExploreStore";
 import { CompanyProfile } from "../company/CompanyProfile";
 import { JobPostFullInfoSkeleton } from "@/components/common/skeleton/JobPostSkeleton";
-import useSessionStore from "@/store/SessionStore";
+import { useSession } from "next-auth/react";
 
 export default function ExploreFullInfo() {
   const exploreType = useExploreStore((state) => state.exploreType);
-  const isSessionLoading = useSessionStore((state) => state.loading);
+  const { status } = useSession();
 
   const selected_job_post = useExploreStore((state) => state.selected_job_post);
   const selected_company = useExploreStore((state) => state.selected_company);
 
   // Show skeleton while loading
-  if (isSessionLoading) {
+  if (
+    status === "loading" ||
+    (!selected_job_post && exploreType === "job-posts") ||
+    (!selected_company && exploreType === "companies")
+  ) {
     return (
       <div className="flex-4 sticky top-28 bottom-4 pb-20 h-fit">
         <div className="max-h-[calc(100vh-9rem)] overflow-y-auto border pb-4 rounded-lg">
