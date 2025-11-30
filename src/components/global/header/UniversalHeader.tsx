f39b537cc704bfeefbb7d76966f84c0d20279dca
mark-ianz <bustillomarkian23@gmail.com>
2025-11-30 18:38:20 +0800

    fix(UniversalHeader): make role prop optional for improved flexibility

--- src/components/global/header/UniversalHeader.tsx
+++ src/components/global/header/UniversalHeader.tsx
@@ -4,7 +4,7 @@ import NavigationMenuUniversal from "./NavigationMenuUniversal";
 import HeaderActions from "./HeaderActions";
 import { Roles } from "@/types/role.types";
 
-export default async function UniversalHeader({ role }: { role: Roles }) {
+export default async function UniversalHeader({ role }: { role?: Roles }) {
   return (
     <header className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50">
       <div className="container mx-auto px-4 lg:px-8">
