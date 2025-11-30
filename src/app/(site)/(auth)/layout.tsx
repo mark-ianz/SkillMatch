b4ec89b5e70fb5f8f6166c805be01e07558363aa
mark-ianz <bustillomarkian23@gmail.com>
2025-11-30 18:37:27 +0800

    feat(AuthLayout): replace HeaderPublic with UniversalHeader for consistent header usage

--- src/app/(site)/(auth)/layout.tsx
+++ src/app/(site)/(auth)/layout.tsx
@@ -1,10 +1,10 @@
+import UniversalHeader from "@/components/global/header/UniversalHeader";
 import { ReactNode } from "react";
-import HeaderPublic from "@/components/global/header/HeaderPublic";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <HeaderPublic />
+      <UniversalHeader/>
       {children}
     </>
   );
