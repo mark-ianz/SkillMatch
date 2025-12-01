faced42a19ef20bd95a8787556c3f164e0776359
mark-ianz <bustillomarkian23@gmail.com>
2025-12-02 03:36:28 +0800

    fix(create_post): update layout styles for improved responsiveness and consistency

--- src/app/(site)/company/(protected)/create-post/page.tsx
+++ src/app/(site)/company/(protected)/create-post/page.tsx
@@ -6,8 +6,8 @@ import React from "react";
 
 export default function CreatePostPage() {
   return (
-    <MainLayout className="items-center">
-      <div className="flex-col w-full max-w-3xl">
+    <MainLayout className="items-center" wrapperClassName="w-full py-10">
+      <div className="flex-col container max-w-5xl">
         <div className="mb-8">
           <h1 className="text-3xl font-bold">Create Post</h1>
           <p className="text-muted-foreground mt-2">
