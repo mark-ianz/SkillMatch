f274a435e0eec1b8471457c212e58f774704d12f
mark-ianz <bustillomarkian23@gmail.com>
2025-12-02 08:36:02 +0800

    feat(ExploreStore): add loading state management for job posts and companies

--- src/store/ExploreStore.ts
+++ src/store/ExploreStore.ts
@@ -7,15 +7,21 @@ interface ExploreStoreState {
   exploreType: ExploreType;
   selected_job_post: JobPost | null;
   selected_company: CompanyProfile | null;
+  isJobPostsLoading: boolean;
+  isCompaniesLoading: boolean;
   setExploreType: (exploreType: ExploreType) => void;
   setSelectedCompany: (company: CompanyProfile | null) => void;
   setSelectedJobPost: (job_post: JobPost | null) => void;
+  setIsJobPostsLoading: (isLoading: boolean) => void;
+  setIsCompaniesLoading: (isLoading: boolean) => void;
 }
 
 export const useExploreStore = create<ExploreStoreState>((set) => ({
   exploreType: "job-postings",
   selected_company: null,
   selected_job_post: null,
+  isJobPostsLoading: false,
+  isCompaniesLoading: false,
   setExploreType: (exploreType) =>
     set({
       exploreType,
@@ -25,4 +31,6 @@ export const useExploreStore = create<ExploreStoreState>((set) => ({
     }),
   setSelectedCompany: (company) => set({ selected_company: company }),
   setSelectedJobPost: (job_post) => set({ selected_job_post: job_post }),
+  setIsJobPostsLoading: (isLoading) => set({ isJobPostsLoading: isLoading }),
+  setIsCompaniesLoading: (isLoading) => set({ isCompaniesLoading: isLoading }),
 }));
