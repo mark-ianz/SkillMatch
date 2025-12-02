7ea9b02c6735c2ac4dad70adefb9c2aa5c4b8819
mark-ianz <bustillomarkian23@gmail.com>
2025-12-02 23:15:33 +0800

    feat(Layout): implement session validation for applicant users

--- src/app/(site)/(applicant)/applicant/layout.tsx
+++ src/app/(site)/(applicant)/applicant/layout.tsx
@@ -1,9 +1,23 @@
 import { ReactNode } from "react";
+import { authConfig } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import Footer from "../../../../components/global/Footer";
 import UniversalHeader from "@/components/global/header/UniversalHeader";
 import MainLayout from "@/components/layout/MainLayout";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+  const session = await getServerSession(authConfig);
+
+  // Check if user is logged in, is an applicant (role_id = 3), and is active (status_id = 1)
+  if (!session || !session.user.user_id) {
+    redirect("/signin");
+  }
+
+  if (session.user.role_id !== 3 || session.user.status_id !== 1) {
+    redirect("/");
+  }
+  
   return (
     <>
       <UniversalHeader role="Applicant"/>
